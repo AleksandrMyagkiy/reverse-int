a456dc47a5a63003b2ecdd2e503011f93cf50ebb
Aleksandr <dentranscom@gmail.com>
2022-01-11 22:56:00 +0200

    feat: solution Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let p = Math.abs(n);
+    let result = 0;
+    while(p) {
+        result = result * 10 + p % 10;
+        p = Math.floor(p / 10);
+    }
+    return result;
+};
